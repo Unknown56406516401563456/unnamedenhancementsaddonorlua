e2484d9763e619151041848959873b4095b41b19
Unknown56406516401563456 <104430315+Unknown56406516401563456@users.noreply.github.com>
2026-03-24 14:28:23 +0800

    Update ueaddonorlua.lua

--- ueaddonorlua.lua
+++ ueaddonorlua.lua
@@ -1,6 +1,3 @@
-function api:set_lua_name(name: string): nil
-function api:get_lua_name(): string
-
 api:set_lua_name("Balls"); -- do not use emojis or special characters.
 print(api:get_lua_name()); -- "my name"
 
