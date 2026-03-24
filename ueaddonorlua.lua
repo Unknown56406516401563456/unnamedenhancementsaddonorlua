aef873f3b846c9dc76548f72bdb32dab188670c3
Unknown56406516401563456 <104430315+Unknown56406516401563456@users.noreply.github.com>
2026-03-24 14:27:07 +0800

    Update ueaddonorlua.lua

--- ueaddonorlua.lua
+++ ueaddonorlua.lua
@@ -3,3 +3,14 @@ function api:get_lua_name(): string
 
 api:set_lua_name("Balls"); -- do not use emojis or special characters.
 print(api:get_lua_name()); -- "my name"
+
+local tabs = {
+    lua = api:AddTab("BallsPriv");
+};
+
+do
+    local groupbox = tabs.lua:AddLeftGroupbox("my groupbox");
+    groupbox:AddToggle("my_toggle", {
+        Text = "toggle", Default = false;
+    });
+end;
